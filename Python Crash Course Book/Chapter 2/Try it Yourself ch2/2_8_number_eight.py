5b6c506afa98c8f126a572e564a7859c7226e7ac
Ayoub Ahmed <ayoub.ahmed.eng@gmail.com>
2026-07-07 02:04:07 +0200

    Add Chapter 2 exercises 2-8 to 2-11

---
+++ Python Crash Course Book/Chapter 2/Try it Yourself ch2/2_8_number_eight.py
@@ -0,0 +1,6 @@
+# 2-9. Favorite Number
+# Store your favorite number in a variable and display it in a message.
+
+favorite_number = 5
+
+print(f"My favorite number is {favorite_number}.")
---
+++ Python Crash Course Book/Chapter 2/Try it Yourself ch2/2_9_favorite_number.py
@@ -0,0 +1,9 @@
+# 2-10. Adding Comments
+# Author: Ayoub Ahmed
+# This program demonstrates how to use comments in Python.
+
+# Store a simple message in a variable.
+message = "Comments make code easier to understand."
+
+# Display the message.
+print(message)
