caa2601c3a41295c9e6f302ab873caebbbc0f8cd
Ayoub Ahmed <ayoub.ahmed.eng@gmail.com>
2026-07-07 02:12:19 +0200

    Rename files for consistent numbering

---
+++ Python Crash Course Book/Chapter 2/Try it Yourself ch2/2_03_personal_message.py
@@ -0,0 +1,7 @@
+# 2-3. Personal Message
+# Use a variable to print a personalized message
+
+name = "Ayoub"
+message = f"Hello {name}, would you like to learn Python today?"
+print(message)
+print(message)
